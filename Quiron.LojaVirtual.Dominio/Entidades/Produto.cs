63997c889a7cf4c6aa292e2061fdf7e8e7ea200b
tercini <raphael.tercini@hotmail.com>
2016-11-22 19:41:28 -0200

    Entity

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Quiron.LojaVirtual.Dominio.Entidade
+namespace Quiron.LojaVirtual.Dominio.Entidades
 {
-    class Produto
+    public class Produto
     {
         public int ProdutoId { get; set; }
         public string Nome { get; set; }
