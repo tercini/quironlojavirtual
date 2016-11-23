a5fc012ad7bafc587799cb01ad6d40d35a73c379
tercini <raphael.tercini@hotmail.com>
2016-11-23 19:57:44 -0200

    Classes de Paginacao

--- Quiron.LojaVirtual.UnitTest/UnitTest1.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Quiron.LojaVirtual.UnitTest
@@ -6,9 +7,29 @@ namespace Quiron.LojaVirtual.UnitTest
     [TestClass]
     public class UnitTestQuiron
     {
-        [TestMethod]
-        public void TestMethod1()
+         [TestMethod]
+        public void Take()
         {
+            int[] numeros = {5, 4, 1, 3, 9, 8, 6, 7, 2, 0};
+            var resultado = from num in numeros.Take(5) select num;
+            int[] teste = {5, 4, 1, 3, 9};
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
         }
+    
+
+        [TestMethod]
+        public void Skip()
+        {
+            //O operador Skip ignora o(s) primeiro(s) n objetos de uma coleção
+            int[] numeros = {5, 4, 1, 3, 9, 8, 6, 7, 2, 0};
+            
+            var resultado = from num in numeros.Take(5).Skip(2) select num;
+
+            int[] teste = {1, 3, 9};
+
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+        }    
+
     }
+
 }
